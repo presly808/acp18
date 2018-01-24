0992d8f05f8aa3780c6195806d3e6411f862b9ef
ShuliakRV <okej@yandex.ua>
2018-01-24 19:09:40 +0200

    task3

--- ShuliakRV/src/main/java/reflection/task1/MyField.java
+++ ShuliakRV/src/main/java/reflection/task1/MyField.java
@@ -1,7 +1,14 @@
 package reflection.task1;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by serhii on 21.01.18.
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface MyField {
 }
--- ShuliakRV/src/main/java/reflection/task1/ReflectionUtils.java
+++ ShuliakRV/src/main/java/reflection/task1/ReflectionUtils.java
@@ -1,6 +1,8 @@
 package reflection.task1;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by serhii on 21.01.18.
@@ -23,8 +25,6 @@ public class ReflectionUtils {
     /*convert all public fields into json string*/
     public static String convertToJson(Object target) {
 
-        Field[] fields = target.getClass().getFields();
-
         StringBuilder res = new StringBuilder();
 
         res.append("{\n");
@@ -33,21 +33,21 @@ public class ReflectionUtils {
 
         String ch = "";
 
-        for (Field f : fields) {
+        for (Field field : target.getClass().getFields()) {
 
             try {
-                value.append(f.get(target).toString());
+                value.append(field.get(target).toString());
             } catch (Exception e) {
                 value.append("null");
             }
 
-            if (f.getType() != String.class) {
+            if (field.getType() != String.class) {
                 ch = "";
             } else {
                 ch = "\"";
             }
 
-            res.append("\"" + f.getName() + "\"" + ":" + ch + value + ch + ",\n");
+            res.append("\"" + field.getName() + "\"" + ":" + ch + value + ch + ",\n");
 
             value.delete(0, value.length());
         }
@@ -57,6 +57,43 @@ public class ReflectionUtils {
     /*convert all fields that were annotated by SpecificAnnotation into json string*/
     public static Object converFromJson(String src, Class cls) {
 
+        String[] lines = src.split("\n");
+
+        Map<String, String> keyValueMap = new HashMap<>();
+
+        for (int i = 1; i < lines.length - 1; i++) {
+            String[] keyValue = lines[i].split(":");
+            keyValueMap.put(keyValue[0].trim(), keyValue[1].trim().replace(",", ""));
+        }
+
+        try {
+
+            Object obj = cls.newInstance();
+
+            for (Field field : cls.getDeclaredFields()) {
+
+                if (field.isAnnotationPresent(MyField.class)) {
+                    String value = keyValueMap.get("\"" + field.getName() + "\"");
+                    if (value != null) {
+
+                        if (field.getType() == int.class) {
+                            field.set(obj, Integer.parseInt(value));
+                        } else {
+                            if (field.getType() == double.class) {
+                                field.set(obj, Double.parseDouble(value));
+                            } else {
+                                if (field.getType() == String.class) {
+                                    field.set(obj, value.replace("\"", ""));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return obj;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
