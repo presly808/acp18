70253461177d53f0ea947cba701ed9683dcfc761
alex323glo <alex323glo@gmail.com>
2018-01-28 12:22:28 +0200

    Complete 2-nd part of Bank task

--- AlexeyOrlyuk/src/main/java/multithreading/bankExample/Bank.java
+++ AlexeyOrlyuk/src/main/java/multithreading/bankExample/Bank.java
@@ -1,5 +1,6 @@
 package multithreading.bankExample;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -9,8 +10,16 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Bank {
 
     private double[] accounts;
-    private Lock lock;
 
+    private Lock lock;
+    private Condition discardCondition;
+
+    /**
+     * Constructor.
+     *
+     * @param n initial number of accounts in this Bank.
+     * @param initialBalance initial balance for each created account in this Bank.
+     */
     public Bank(int n, double initialBalance) {
         accounts = new double[n];
         for (int i = 0; i < accounts.length; i++){
@@ -18,26 +27,52 @@ public class Bank {
         }
 
         lock = new ReentrantLock(true);
+        discardCondition = lock.newCondition();
     }
 
+    /**
+     * Carries out inside-Bank transaction.
+     *
+     * @param from index of account-sender in accounts' array.
+     * @param to index of account-receiver in accounts' array.
+     * @param amount funds amount, which will be discounted from sender's balance
+     *               and will be added to receiver's balance.
+     */
     public void transfer(int from, int to, double amount) {
 
-        lock.lock();
+        try {
 
-        if (accounts[from] < amount) {
+            lock.lock();
+
+            while (accounts[from] < amount) {
+                try {
+                    System.out.print(Thread.currentThread());
+                    System.out.printf(" Can't send %10.2f from %d to %d (to small balance).\n", amount, from, to);
+                    discardCondition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" %10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+
+            discardCondition.signalAll();
+
+        } finally {
             lock.unlock();
-            return;
         }
-        System.out.print(Thread.currentThread());
-        accounts[from] -= amount;
-        System.out.printf(" %10.2f from %d to %d", amount, from, to);
-        accounts[to] += amount;
-        System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
-
-        lock.unlock();
 
     }
 
+    /**
+     * Shows total amount of this Bank.
+     *
+     * @return sum of amounts of all accounts in this Bank.
+     */
     private double getTotalBalance() {
         double sum = 0;
         for (double a: accounts){
@@ -46,6 +81,11 @@ public class Bank {
         return sum;
     }
 
+    /**
+     * Shows this Bank's "size".
+     *
+     * @return number of accounts (length of accounts Array).
+     */
     public int size(){
         return accounts.length;
     }
--- AlexeyOrlyuk/src/main/java/multithreading/bankExample/TransferRunnable.java
+++ AlexeyOrlyuk/src/main/java/multithreading/bankExample/TransferRunnable.java
@@ -9,22 +9,36 @@ public class TransferRunnable implements Runnable {
     private double maxAmount;
     private int DELAY = 10;
 
+    private int operationCount;
+
     public TransferRunnable(Bank b, int from, double max) {
         bank = b;
         fromAccount = from;
         maxAmount = max;
     }
 
+    public TransferRunnable(Bank b, int from, double max, int operationCount) {
+        bank = b;
+        fromAccount = from;
+        maxAmount = max;
+
+        this.operationCount = operationCount;
+    }
+
     @Override
     public void run() {
 
         try {
+            int i = 0;
             while (true){
                 int toAccount = (int) (bank.size()*Math.random());
                 double amount = maxAmount*Math.random();
                 bank.transfer(fromAccount, toAccount, amount);
                 Thread.sleep((int) (DELAY*Math.random()));
 
+                if (operationCount > 0 && i++ >= operationCount) {
+                    break;
+                }
             }
         }
         catch (InterruptedException e){}
