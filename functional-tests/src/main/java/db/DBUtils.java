5b217efe06ef91e3086916537631c4fdf04db9fb
presly808 <presly808@gmail.com>
2018-02-03 13:05:07 +0200

    db task has been added

---
+++ functional-tests/src/main/java/db/DBUtils.java
@@ -0,0 +1,102 @@
+package db;
+
+import db.model.City;
+import db.model.Department;
+import db.model.User;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by serhii on 03.02.18.
+ */
+public class DBUtils implements IDB {
+
+    private String url;
+
+    public DBUtils(String url) {
+        this.url = url;
+    }
+
+    public List<User> getAll(){
+        return null;
+    }
+
+    @Override
+    public List<User> selectWithFilter(Map<Field, Object> filters, boolean orderBy, int limit) {
+        return null;
+    }
+
+    @Override
+    public boolean createTableAndFill(String csvUrl) {
+        return false;
+    }
+
+    @Override
+    public boolean fillTable(String csvUrl) {
+        return false;
+    }
+
+    @Override
+    public boolean createTable(Class clazz) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAllValues(Class clazz) {
+        return false;
+    }
+
+    @Override
+    public Map<Department, List<User>> getUsersGroupByDepartment() {
+        return null;
+    }
+
+    @Override
+    public Map<Department, Integer> getAvgSalaryGroupByDepartment() {
+        return null;
+    }
+
+    @Override
+    public Map<User, List<User>> getUsersGroupByManagersAndOrderedThatLiveInKiev() {
+        return null;
+    }
+
+    @Override
+    public User addUser(User userWithoutId) {
+        return null;
+    }
+
+    @Override
+    public User removeUser(User user) {
+        return null;
+    }
+
+    @Override
+    public City addCity(City city) {
+        return null;
+    }
+
+    @Override
+    public City removeCity(City city) {
+        return null;
+    }
+
+    @Override
+    public Department addDepart(Department department) {
+        return null;
+    }
+
+    @Override
+    public Department removeDepart(Department department) {
+        return null;
+    }
+
+    @Override
+    public boolean dropTable(Class clazz) {
+        return false;
+    }
+
+
+}
