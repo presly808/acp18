29e0ac96dc7f62011dfc151b2b03b41b6542561c
presly808 <presly808@gmail.com>
2018-02-04 12:44:41 +0200

    added more tests

--- functional-tests/src/test/java/db/IDBTest.java
+++ functional-tests/src/test/java/db/IDBTest.java
@@ -18,13 +18,20 @@ import static org.junit.Assert.*;
  */
 public class IDBTest {
     // todo add your implementation
-    private static IDB idb;
+    private static IDB idb = new DBUtils("./database.db");
 
     @BeforeClass
     public static void createtables() {
         idb.createTable(Department.class);
         idb.createTable(City.class);
         idb.createTable(User.class);
+    }
+
+    @AfterClass
+    public static void dropTables() {
+        idb.dropTable(User.class);
+        idb.dropTable(Department.class);
+        idb.dropTable(City.class);
     }
 
     @Before
@@ -44,8 +51,8 @@ public class IDBTest {
 
 
         Department department2 = new Department();
-        department2.setId(1);
-        department2.setName("IT");
+        department2.setId(2);
+        department2.setName("QA");
 
         User user3 = new User(3, "Yura", 35, 1500, department2, kiev, null);
         User user1 = new User(1, "Ivan", 30, 2500, department2, kiev, user3);
@@ -87,16 +94,6 @@ public class IDBTest {
     }
 
     @Test
-    public void fillTable() throws Exception {
-
-    }
-
-    @Test
-    public void createTable() throws Exception {
-
-    }
-
-    @Test
     public void getUsersGroupByDepartment() throws Exception {
         Map<Department, List<User>> usersGroupByDepartment = idb.getUsersGroupByDepartment();
         Assert.assertThat(usersGroupByDepartment.keySet().size(), CoreMatchers.equalTo(2));
@@ -119,9 +116,9 @@ public class IDBTest {
 
     @Test
     public void addUser() throws Exception {
-        User yura = new User(3, "Yura", 35, 1500, null, null, null);
+        User yura = new User(120, "TestUser", 35, 1500, null, null, null);
         User actual = idb.addUser(yura);
-        Assert.assertThat(actual.getName(), CoreMatchers.equalTo("Yura"));
+        Assert.assertThat(actual.getName(), CoreMatchers.equalTo("TestUser"));
     }
 
     @Test
@@ -134,41 +131,41 @@ public class IDBTest {
     @Test
     public void addCity() throws Exception {
         City kiev = new City();
-        kiev.setId(1);
-        kiev.setName("Kiev");
+        kiev.setId(5);
+        kiev.setName("Chabany");
 
         City city = idb.addCity(kiev);
 
-
-        Assert.assertThat(city.getId(), CoreMatchers.equalTo(1));
+        Assert.assertThat(city.getId(), CoreMatchers.equalTo(5));
     }
 
     @Test
     public void addDepart() throws Exception {
 
         Department department1 = new Department();
-        department1.setId(1);
-        department1.setName("IT");
+        department1.setId(8);
+        department1.setName("TESTDEPART");
 
         Department department = idb.addDepart(department1);
-        Assert.assertThat(department.getId(), CoreMatchers.equalTo(1));
+        Assert.assertThat(department.getId(), CoreMatchers.equalTo(8));
 
-    }
-
-    @Test
-    public void dropTable() throws Exception {
-    }
-
-    @Test
-    public void removeAllValues() throws Exception {
     }
 
     @Test
     public void removeCity() throws Exception {
+        City city = new City();
+        city.setId(1);
+        City ret = idb.removeCity(city);
+        Assert.assertThat(ret.getName(), CoreMatchers.equalTo("Kiev"));
     }
 
     @Test
     public void removeDepart() throws Exception {
+        Department department = new Department();
+        department.setId(1);
+        Department rem = idb.removeDepart(department);
+
+        Assert.assertThat(rem.getName(), CoreMatchers.is("QA"));
     }
 
 }
