93f641e99cc227cea43fe689a4916351c6bd8d02
alex323glo <alex323glo@gmail.com>
2018-02-06 01:52:41 +0200

    Add Unit tests for DBUtils interface (using MySQL), create implementation of DBUtils interface

---
+++ AlexeyOrlyuk/src/main/java/db/utils/DBUtilsImpl.java
@@ -0,0 +1,184 @@
+package db.utils;
+
+import java.lang.reflect.Field;
+import java.sql.*;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Implementation of Data Base utils container (DAO).
+ *
+ * @author alex323glo
+ * @version 1.0
+ *
+ * @see DBUtils
+ */
+public class DBUtilsImpl implements DBUtils {
+
+    private String url;
+    private String user;
+    private String password;
+
+    public DBUtilsImpl(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    /**
+     * Creates new table in DB.
+     *
+     * @param tableClass type, which describes table records as objects (used to extract table name).
+     * @return true, if operation was successful.
+     */
+    @Override
+    public boolean createTable(Class tableClass) {
+        if (tableClass == null) {
+            return false;
+        }
+
+        try (Statement statement = DriverManager
+                .getConnection(url, user, password)
+                .createStatement();
+                ) {
+
+            return !statement.execute(ReflectionUtils.createSQLTableStructure(tableClass));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    /**
+     * Drops (deletes) table from DB.
+     *
+     * @param tableClass type, which describes table records as objects (used to extract table name).
+     * @return true, if operation was successful.
+     */
+    @Override
+    public boolean dropTable(Class tableClass) {
+        if (tableClass == null) {
+            return false;
+        }
+
+        try (Statement statement = DriverManager
+                .getConnection(url, user, password)
+                .createStatement()
+                ) {
+
+            return !statement.execute("DROP TABLE " + ReflectionUtils.createTableName(tableClass) + ";");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    /**
+     * Removes all records from table.
+     *
+     * @param tableClass type, which describes table records as objects (used to extract table name).
+     * @return true, if operation was successful.
+     */
+    @Override
+    public boolean removeAllValues(Class tableClass) {
+        if (tableClass == null) {
+            return false;
+        }
+
+        try (Statement statement = DriverManager
+                .getConnection(url, user, password)
+                .createStatement()
+                ) {
+
+            return !statement.execute("DELETE FROM " + ReflectionUtils.createTableName(tableClass) + ";");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    /**
+     * Executes native SQL query in current DB.
+     *
+     * @param sql String SQL query.
+     * @return String result of query execution.
+     */
+    @Override
+    public String nativeSQL(String sql) {
+        if (sql == null) {
+            return null;
+        }
+
+        try (Statement statement = DriverManager
+                .getConnection(url, user, password)
+                .createStatement()
+                ) {
+
+            statement.execute(sql);
+            return Converter.resultSetToString(statement.getResultSet());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets all records of table.
+     *
+     * @param tableClass type, which describes table records as objects (used to extract table name).
+     * @return List of records (tableClass objects).
+     */
+    @Override
+    public <T> List<T> getAllValues(Class<T> tableClass) {
+        // TODO complete
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Selects records from table using filters.
+     *
+     * @param tableClass type, which describes table records as objects (used to extract table name).
+     * @param filters    Map of field filters.
+     * @param orderBy    target field, which is used for ORDER BY SQL operation.
+     * @param limit      value, used for LIMIT SQL operation.
+     * @return result List of records (tableClass objects).
+     */
+    @Override
+    public <T> List<T> selectWithFilter(Class<T> tableClass, Map<Field, Object> filters, Field orderBy, int limit) {
+        // TODO complete
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Adds new record to table.
+     *
+     * @param tableClass  type, which describes table records as objects (used to extract table name).
+     * @param tableRecord tableClass object, which data will be added to table as new record.
+     * @return tableClass object of added record, if operation was successful, or null, if it wasn't.
+     */
+    @Override
+    public <T> T add(Class<T> tableClass, T tableRecord) {
+        // TODO complete
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @param tableClass  type, which describes table records as objects (used to extract table name).
+     * @param tableRecord tableClass object, which data will be used to identify record,
+     *                    needed to be removed from table.
+     * @return tableClass object of removed record, if operation was successful, or null, if it wasn't.
+     */
+    @Override
+    public <T> T remove(Class<T> tableClass, T tableRecord) {
+        // TODO complete
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ AlexeyOrlyuk/src/test/java/db/utils/DBUtilsTestWithMySQL.java
@@ -0,0 +1,212 @@
+package db.utils;
+
+import db.model.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class DBUtilsTestWithMySQL {
+
+    private static DBUtils dbUtils;
+
+    private static final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+
+    private static final String URL = "jdbc:mysql://localhost:3306/ACP18";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    private static boolean tableIsCreated = false;
+
+    @Before
+    public void setUp() throws Exception {
+        Class.forName(DRIVER_CLASS_NAME);
+        dbUtils = new DBUtilsImpl(URL, USER, PASSWORD);
+
+        nativelyCreateUserTable();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        nativelyDropUserTable();
+    }
+
+    @Test
+    public void createTable() throws Exception {
+        nativelyDropUserTable();    // to support @Before
+
+        assertTrue(dbUtils.createTable(User.class));
+    }
+
+    @Test
+    public void dropTable() throws Exception {
+        assertTrue(dbUtils.dropTable(User.class));
+
+        nativelyCreateUserTable();  // to support @After
+    }
+
+    @Test
+    public void removeAllValues() throws Exception {
+        String nativeInsertQuery1 = "INSERT INTO User(id, name) VALUES(1, 'Alex');";
+        String nativeInsertQuery2 = "INSERT INTO User(id, name) VALUES(2, 'Max');";
+        dbUtils.nativeSQL(nativeInsertQuery1);
+        dbUtils.nativeSQL(nativeInsertQuery2);
+
+        assertTrue(dbUtils.removeAllValues(User.class));
+
+        String actualResult = null;
+
+        try (
+                ResultSet resultSet = DriverManager
+                        .getConnection(URL, USER, PASSWORD)
+                        .createStatement()
+                        .executeQuery("SELECT * FROM User;")
+                ) {
+
+            actualResult = Converter.resultSetToString(resultSet);
+        }
+
+        assertEquals(0, actualResult.length());
+    }
+
+    @Test
+    public void nativeSQL() throws Exception {
+        String nativeInsertQuery = "INSERT INTO User(id, name) VALUES(1, 'Alex');";
+        String nativeSelectQuery = "SELECT id, name from User;";
+        String expectedResult = "1 Alex";
+
+        assertNull(dbUtils.nativeSQL(nativeInsertQuery));
+        assertEquals(expectedResult, dbUtils.nativeSQL(nativeSelectQuery));
+    }
+
+    // TODO complete implementation
+    @Ignore
+    @Test
+    public void getAllValues() throws Exception {
+        List<User> expectedList = new ArrayList<>();
+        expectedList.add(new User(1, "Alex", 20));
+        expectedList.add(new User(2, "John", 21));
+
+        try (
+                Statement statement = DriverManager
+                        .getConnection(URL, USER, PASSWORD)
+                        .createStatement()
+                ) {
+            statement.execute("INSERT INTO User (id, name, age) VALUES (1, 'Alex', 20);");
+            statement.execute("INSERT INTO User (id, name, age) VALUES (2, 'John', 21);");
+        }
+
+        List<User> actualList = dbUtils.getAllValues(User.class);
+
+        assertEquals(expectedList, actualList);
+    }
+
+    // TODO complete implementation
+    @Ignore
+    @Test
+    public void selectWithFilter() throws Exception {
+    }
+
+    // TODO complete implementation
+    @Ignore
+    @Test
+    public void add() throws Exception {
+        User user = new User(1, "Alex", 20);
+        assertEquals(user, dbUtils.add(User.class, user));
+
+        String expectedResult = "1 Alex 20";
+        String actualResult = null;
+
+        try (
+                ResultSet resultSet = DriverManager
+                        .getConnection(URL, USER, PASSWORD)
+                        .createStatement()
+                        .executeQuery("SELECT id, name, age FROM User;")
+        ) {
+            actualResult = Converter.resultSetToString(resultSet);
+        }
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    // TODO complete implementation
+    @Ignore
+    @Test
+    public void remove() throws Exception {
+
+        User expectedUser = new User(1, "Alex", 20);
+
+        try (
+                Statement statement = DriverManager
+                        .getConnection(URL, USER, PASSWORD)
+                        .createStatement()
+        ) {
+            statement.execute("INSERT INTO User (id, name, age) VALUES (1, 'Alex', 20);");
+        }
+
+        User removedUser = dbUtils.remove(User.class, expectedUser);
+        assertEquals(expectedUser, removedUser);
+
+        String actualResult = null;
+
+        try (
+                ResultSet resultSet = DriverManager
+                        .getConnection(URL, USER, PASSWORD)
+                        .createStatement()
+                        .executeQuery("SELECT * FROM User;")
+        ) {
+            actualResult = Converter.resultSetToString(resultSet);
+        }
+
+        assertEquals(0, actualResult.length());
+    }
+
+    private static void nativelyCreateUserTable() {
+        try (
+                Statement statement = DriverManager
+                        .getConnection(URL, USER, PASSWORD)
+                        .createStatement()
+                ) {
+
+            statement.execute(
+                    "CREATE TABLE User (\n" +
+                            "id INT,\n" +
+                            "name VARCHAR(255),\n" +
+                            "salary DOUBLE,\n" +
+                            "age INT,\n" +
+                            "department_id INT,\n" +
+                            "city_id INT,\n" +
+                            "manage_id INT,\n" +
+                            "PRIMARY KEY (id)\n" +
+                            ");"
+            );
+            System.out.println("@Before:setUp(): Table User was successfully created!\n");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void nativelyDropUserTable() {
+        try (
+                Statement statement = DriverManager
+                        .getConnection(URL, USER, PASSWORD)
+                        .createStatement()
+        ) {
+
+            statement.execute("DROP TABLE User;");
+            System.out.println("@After:tearDown(): Table User was successfully dropped!\n");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
