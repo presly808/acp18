0fd9bbfb3920aa6baba33498fb4cc9fa6b30779c
WIN <WIN72007@ukr.net>
2018-02-10 14:22:10 +0200

    hibernate task

---
+++ Mykhailechko/src/main/java/hibernate/model/Base.java
@@ -0,0 +1,49 @@
+package hibernate.model;
+
+/**
+ * Created by serhii on 03.02.18.
+ */
+public class Base {
+
+    protected int id;
+    protected String name;
+
+    public Base() {
+    }
+
+    public Base(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Base base = (Base) o;
+
+        return id == base.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
