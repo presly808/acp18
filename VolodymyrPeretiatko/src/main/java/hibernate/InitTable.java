8738dce248cbac61201bb2dac753ad327e052fb8
PeretyatkoVladimir <voc@ukr.net>
2018-02-11 13:15:25 +0200

    before merege with master

--- VolodymyrPeretiatko/src/main/java/hibernate/InitTable.java
+++ VolodymyrPeretiatko/src/main/java/hibernate/InitTable.java
@@ -1,11 +1,15 @@
 package hibernate;
 
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class InitTable {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hibernate-unit");
+        EntityManager entityMng =  entityManagerFactory.createEntityManager();
+        entityMng.close();
+        entityManagerFactory.close();
     }
 }
