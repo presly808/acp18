9602258320cdc006f1ebad42e5a93a6ccca1f920
ShuliakRV <okej@yandex.ua>
2018-02-13 22:41:12 +0200

    hibernate configuration

--- ShuliakRV/src/main/java/hibernate/service/Test.java
+++ ShuliakRV/src/main/java/hibernate/service/Test.java
@@ -1,4 +1,12 @@
 package hibernate.service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Test {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("hibernate-unit");
+        EntityManager manager = factory.createEntityManager();
+    }
 }
