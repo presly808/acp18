f9bda81075d8af138389c92d0fea428efac37947
smikhun <a.smikhun@gmail.com>
2018-02-20 20:16:59 +0200

    Fix bag resources and create DAo

---
+++ AndriySmikhun/src/main/java/hibernate/dao/DaoUserImpl.java
@@ -0,0 +1,31 @@
+package hibernate.dao;
+
+import java.util.List;
+
+public class DaoUserImpl implements Dao {
+
+    @Override
+    public List findAll() {
+        return null;
+    }
+
+    @Override
+    public List findAll(int offset, int length) {
+        return null;
+    }
+
+    @Override
+    public Object find(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object entity) {
+        return null;
+    }
+}
