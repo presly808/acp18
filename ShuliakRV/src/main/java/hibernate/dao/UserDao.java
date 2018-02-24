9e3040ca99fe7639a915ae40b738591a2bd41194
ShuliakRV <okej@yandex.ua>
2018-02-25 01:02:54 +0200

    hibernate task

--- ShuliakRV/src/main/java/hibernate/dao/UserDao.java
+++ ShuliakRV/src/main/java/hibernate/dao/UserDao.java
@@ -15,7 +15,7 @@ public class UserDao extends DaoImpl<User, Integer> {
 
     public List<User> findByName(String name) {
 
-        logger.info("Finding User by name!");
+        logger.info("Finding Users by name!");
 
         String queryUser = "SELECT u FROM User u " +
                 "WHERE u.name = :name";
@@ -32,4 +32,27 @@ public class UserDao extends DaoImpl<User, Integer> {
             manager.close();
         }
     }
+
+    public List<User> findInRange(double minSal, double maxSal) {
+
+        logger.info("Finding Users salary range!");
+
+        String queryUser = "SELECT u FROM User u " +
+                "WHERE u.salary between :minSal AND :maxSal";
+
+        EntityManager manager = factory.createEntityManager();
+        Query resQuery = manager.createQuery(queryUser);
+        resQuery.setParameter("minSal", minSal);
+        resQuery.setParameter("maxSal", maxSal);
+        try {
+            return resQuery.getResultList();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return null;
+        } finally {
+            manager.close();
+        }
+    }
+
+
 }
