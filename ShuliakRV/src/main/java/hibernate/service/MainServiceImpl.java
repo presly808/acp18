09914b72dbc284eeb4eb52e02a4aac26f5604e53
ShuliakRV <okej@yandex.ua>
2018-02-25 00:06:33 +0200

    hibernate task

--- ShuliakRV/src/main/java/hibernate/service/MainServiceImpl.java
+++ ShuliakRV/src/main/java/hibernate/service/MainServiceImpl.java
@@ -1,9 +1,15 @@
 package hibernate.service;
 
+import hibernate.dao.Dao;
+import hibernate.dao.DaoImpl;
 import hibernate.exception.AppException;
 import hibernate.model.Department;
 import hibernate.model.User;
+import spring.UserDao;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +18,17 @@ import java.util.Map;
  * Created by serhii on 10.02.18.
  */
 public class MainServiceImpl implements MainService {
+
+    private Dao<User, Integer> userDao;
+    private Dao<Department, Integer> departmentDao;
+    private EntityManagerFactory factory;
+
+    public MainServiceImpl() {
+        factory = Persistence.createEntityManagerFactory("hibernate-unit");
+        userDao = new DaoImpl<User, Integer>(factory);
+        departmentDao = new DaoImpl<Department, Integer>(factory);
+    }
+
     @Override
     public User register(User user) throws AppException {
         return null;
