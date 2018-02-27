424ab04d91c133c5befd691e48ddc5a66349747c
ShuliakRV <okej@yandex.ua>
2018-02-27 15:53:40 +0200

    hibernate task

--- ShuliakRV/src/test/java/hibernate/service/MainServiceImplTest.java
+++ ShuliakRV/src/test/java/hibernate/service/MainServiceImplTest.java
@@ -160,6 +160,37 @@ public class MainServiceImplTest {
 
     @Test
     public void findInRange() throws Exception {
+
+        City kiev = new City();
+        kiev.setName("Kiev");
+
+        City odessa = new City();
+        odessa.setName("Oddessa");
+
+        Department department1 = new Department();
+        department1.setName("IT");
+
+        Department department2 = new Department();
+        department2.setName("QA");
+
+        User user3 = new User("Yura", 35, 1500, department2, kiev, null);
+        User user1 = new User("Ivan", 30, 2500, department2, kiev, user3);
+        User user2 = new User( "Oleg", 33, 3500, department2, odessa, user3);
+        User user4 = new User( "Serhii", 22, 2500, department1, kiev, user3);
+        User user5 = new User( "Olex", 24, 4500, department1, odessa, user3);
+
+        service.addCity(kiev);
+        service.addCity(odessa);
+        service.addDepartment(department1);
+        service.addDepartment(department2);
+        service.register(user3);
+        service.register(user1);
+        service.register(user2);
+        service.register(user4);
+        service.register(user5);
+
+        assertEquals(3, service.findInRange(1000,2500).size());
+
     }
 
     @Test
