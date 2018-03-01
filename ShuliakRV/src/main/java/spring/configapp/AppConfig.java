dc841d4b602703fdc27c206db925ab02f8a246b4
ShuliakRV <okej@yandex.ua>
2018-03-02 01:44:47 +0200

    spring task

--- ShuliakRV/src/main/java/spring/configapp/AppConfig.java
+++ ShuliakRV/src/main/java/spring/configapp/AppConfig.java
@@ -2,18 +2,29 @@ package spring.configapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class AppConfig {
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
         bean.setPackagesToScan("spring.model");
         bean.setPersistenceUnitName("spring-unit");
         bean.setPersistenceXmlLocation("classpath:/META-INF/persistence.xml");
         bean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
         return bean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return jpaTransactionManager;
+
     }
 }
